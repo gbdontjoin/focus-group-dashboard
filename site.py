22c29d9a3b3d00875d2735083206fee794f37d3d
gbdontjoin <gregbarton1001@gmail.com>
2025-05-20 03:38:17 -0400

    Link Update site.py

--- site.py
+++ site.py
@@ -1,16 +1,21 @@
 import streamlit as st
-import webbrowser
 
 st.title("🎯 Focus Group Dashboard")
 
+st.markdown("""
+<a href="https://www.findfocusgroups.com/remote" target="_blank">
+    <button style='font-size:16px; padding:10px 24px;'>Open FindFocusGroups</button>
+</a>
+""", unsafe_allow_html=True)
 
-if st.button("Open FindFocusGroups"):
-    webbrowser.open("https://www.findfocusgroups.com/paid-focus-groups/new-york-city/")
+st.markdown("""
+<a href="https://www.respondent.io/projects" target="_blank">
+    <button style='font-size:16px; padding:10px 24px;'>Open Respondent</button>
+</a>
+""", unsafe_allow_html=True)
 
-if st.button("Open App . Respondent"):
-    webbrowser.open("https://app.respondent.io/respondents/v2/dashboard/")
-
-if st.button("Open User Interviews"):
-    webbrowser.open("https://www.userinterviews.com/studies?sort=-id")
-
-st.text_area("Paste links here to track")
+st.markdown("""
+<a href="https://www.userinterviews.com/projects" target="_blank">
+    <button style='font-size:16px; padding:10px 24px;'>Open User Interviews</button>
+</a>
+""", unsafe_allow_html=True)
